d4f7eccb31d776fed22ae35ece382cb9451eeb9c
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-16 16:46:51 -0900

    Declared variables to manipulate DOM in JavaScript

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const imagesArray = document.getElementsByTagName("img"); //Stores the images hidden by divs
-const  tilesArray = document.getElementsByTagName("div");
+const  tilesArray = document.getElementsByTagName("div"); //Our div tiles are stored in this array
+let scoreCount = document.getElementById("score");  //Stores the score
+let failedAttemptsCount = document.getElementById("failedAttempts");
+
+//Initialise the score to 0
+scoreCount.value = 0;
+//Initialise failed attempts to 0
+failedAttemptsCount.value = 0;
 
