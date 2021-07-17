e78e80d1f7ab7be7d0712f7271cdfa76ec936cfa
Sibusiso Ndzala <sw.ndzala@gmail.com>
2021-07-16 16:49:38 -0900

    comment failedAttempt variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const imagesArray = document.getElementsByTagName("img"); //Stores the images hidden by divs
 const  tilesArray = document.getElementsByTagName("div"); //Our div tiles are stored in this array
 let scoreCount = document.getElementById("score");  //Stores the score
-let failedAttemptsCount = document.getElementById("failedAttempts");
+let failedAttemptsCount = document.getElementById("failedAttempts");//Store the number of failed attempts
 
 //Initialise the score to 0
 scoreCount.value = 0;
